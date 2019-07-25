d7910b11dcbc481c5688fb91e0ea16ec0d596633
Jeev Arumugam <jeevaweb17@gmail.com>
2019-07-25 13:51:56 -0400

    finished upto githubrepos

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,14 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
-const initialState = [];
-
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT,
+  ACCOUNT_DELETED
+} from '../actions/types';
+const initialState = {};
 const initalState = {
   token: localStorage.getItem('token'),
   isAuthenticated: null,
@@ -11,7 +19,15 @@ const initalState = {
 export default function(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -20,6 +36,10 @@ export default function(state = initialState, action) {
         loading: false
       };
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case AUTH_ERROR:
+    case LOGOUT:
+    case ACCOUNT_DELETED:
       localStorage.removeItem('token');
       return {
         ...state,
